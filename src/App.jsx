d9b5aeff56e7780eed8e6c8f6938b4f25c624aab
Manuel Arenas <maarenasl@eafit.edu.co>
2025-03-26 12:13:16 -0500

    Primer commit para el trabajo hecho en youtube

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react"
+import './App.css'
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+//const CAT_ENDPOINT_IMAGE_URL = 'https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true'
+
+export function App() {
+    const [fact, setFact] = useState()
+    const [imageUrl, setImageUrl] = useState()
+
+    useEffect(() => {
+        fetch(CAT_ENDPOINT_RANDOM_FACT)
+            .then(res => res.json())
+            .then(data => {
+                const {fact} = data
+                setFact(fact)
+
+                const threefirstWord = fact.split(' ', 3).join(' ')
+
+                fetch(`https://cataas.com/cat/says/${threefirstWord}?size=50&color=red&json=true`)
+                    .then(res => res.json())
+                    .then(response => {
+                        const {url} = response
+                        setImageUrl(url)
+                    })
+
+            })
+    }, [])
+
+    return (
+        <main>
+            <h1>App de gatitos</h1>
+            {fact && <p>{fact}</p>} 
+            {imageUrl && <img src={imageUrl} alt={'Image extracted using the first using the first word of the fact'} />}
+            
+            
+        </main>
+
+    )
+}
